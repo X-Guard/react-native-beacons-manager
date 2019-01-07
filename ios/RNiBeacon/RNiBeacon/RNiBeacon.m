feaaccad9bd57631606060516b7701c4bc2b15cb
Jesse Degger <jesse@socialsquad.nl>
2019-01-07 12:11:06 +0100

    add fix

--- ios/RNiBeacon/RNiBeacon/RNiBeacon.m
+++ ios/RNiBeacon/RNiBeacon/RNiBeacon.m
@@ -257,6 +257,11 @@ RCT_EXPORT_METHOD(shouldDropEmptyRanges:(BOOL)drop)
 
 -(void)locationManager:(CLLocationManager *)manager
         didEnterRegion:(CLBeaconRegion *)region {
+
+  if (! [region respondsToSelector:@selector(proximityUUID)]) {
+    return;
+  }
+
   NSDictionary *event = @{
                           @"identifier": region.identifier,
                           @"uuid": [region.proximityUUID UUIDString],
@@ -267,6 +272,11 @@ RCT_EXPORT_METHOD(shouldDropEmptyRanges:(BOOL)drop)
 
 -(void)locationManager:(CLLocationManager *)manager
          didExitRegion:(CLBeaconRegion *)region {
+
+  if (! [region respondsToSelector:@selector(proximityUUID)]) {
+    return;
+  }
+  
   NSDictionary *event = @{
                           @"identifier": region.identifier,
                           @"uuid": [region.proximityUUID UUIDString],
